788e62ef291253f7ce389129d4087f70ee530e8e
Victor Pradom <vmpradob@gmail.com>
2021-09-29 20:57:20 -0400

    added nokia commands

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import sys
 import textfsm
 from tabulate import tabulate
 template = './templates/show_bgp_ipv4_all_neighbors_performance-statistics.textfsm'
-output_file = './files/show_bgp_ipv4_all_neighbors_performance-statistics.txt'
+output_file = './files/cisco_commands/show_bgp_ipv4_all_neighbors_performance-statistics.txt'
 with open(template) as f, open(output_file) as output:
     re_table = textfsm.TextFSM(f)
     header = re_table.header
