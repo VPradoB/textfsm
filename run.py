91a69c54339e52ce96e45d427c36acd726540ee1
Victor Prado <vmpradob@gmail.com>
2021-11-03 16:21:04 -0400

    commit de ejemplo para mostrar como funciona git

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 import os
 import textfsm
 from tabulate import tabulate
+
 template = 'templates/cisco_ios/show_ip_bgp_neighbor,textfsm'
 command_file = 'files/cisco_ios/show_ip_bgp_neighbor'
 output_path = 'out/cisco_ios/show_ip_bgp_neighbor.out'
+
 with open(template) as t, open(command_file) as c:
     with open(output_path, 'w') as o:
         re_table = textfsm.TextFSM(t)
