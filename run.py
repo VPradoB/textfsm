6a966486659b3055ca3c4a3f93e73aec2b999137
Victor Prado <vmpradob@gmail.com>
2021-11-18 22:20:03 -0400

    add new commands

--- run.py
+++ run.py
@@ -2,15 +2,15 @@ import os
 import textfsm
 from tabulate import tabulate
 
-template = 'templates/cisco_ios/show_ip_bgp_neighbor,textfsm'
-command_file = 'files/cisco_ios/show_ip_bgp_neighbor'
-output_path = 'out/cisco_ios/show_ip_bgp_neighbor.out'
+template = 'templates/cisco_xr/cisco_xr_show_ip_bgp_neighbors.textfsm'
+command_file = 'files/cisco_xr/show_ip_bgp_neighbor'
+output_path = 'out/cisco_xr/show_ip_bgp_neighbor.out'
 
 with open(template) as t, open(command_file) as c:
     with open(output_path, 'w') as o:
         re_table = textfsm.TextFSM(t)
         header = re_table.header
         p = c.read()
-        result = re_table.ParseText(p)
+        result = tabulate(re_table.ParseText(p), headers=header)
         print(result, file=o)
-        print(tabulate(result, headers=header))
+        print(result)
