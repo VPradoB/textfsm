f0c68977b1d40eb2cab4d2dda7b749433e6ee861
Victor Prado <vmpradob@gmail.com>
2021-11-24 09:40:34 -0400

    solved duplicated data issue

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 import textfsm
 from tabulate import tabulate
 
-template = "templates/cisco_ios_xe/sh_ip_route_summary.textfsm"
-command_file = "files/cisco_ios_xe/sh_ip_route_summary"
-output_path = "out/cisco_ios_xe/sh_ip_route_summary.out"
+template = "templates/cisco_xr/show_processes_cpu.textfsm"
+command_file = "files/cisco_xr/show_processes_cpu"
+output_path = "out/cisco_xr/show_processes_cpu.out"
 
 with open(template) as t, open(command_file) as c:
     with open(output_path, "w") as o:
@@ -11,6 +11,6 @@ with open(template) as t, open(command_file) as c:
         header = re_table.header
         p = c.read()
         raw_result = re_table.ParseText(p)
-        result = tabulate(re_table.ParseText(p), headers=header)
+        result = tabulate(raw_result, headers=header)
         print(result, file=o)
         print(result)
